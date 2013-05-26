154a580640147138944c8455aff73a52b347d2cc
Berk D. Demir <bdd@mindcast.org>
2013-05-26 16:13:46 -0700

    Source everything from (M)ELPA. Separate packages
    Following the 'lisp' directory, also drop 'themes', installing "Tomorrow" theme
    from MELPA.
    
    Move all third party package descriptions done via `use-package' to a separate
    file `packages.el' and also assign it a register to easier jumping to it when
    necessary.

--- init.el
+++ init.el
@@ -13,14 +13,14 @@
   `(dolist (hook ,mode-hooks)
      (add-hook hook ,function)))
 
-;;; Load 'load-path.el' so we know where to load from.
-(load (emacs-d "load-path"))
-;;; Personal elisp functions.
+;;; External Packages
+(load (emacs-d "packages"))
+;;; Personal Elisp functions
 (load (emacs-d "bdd-defuns"))
 ;;; Twitter
 (load (emacs-d "twitter") 'missing-ok)
 ;;; Theme
-(load-theme 'tomorrow-night-eighties t)
+(load-theme 'sanityinc-tomorrow-eighties t)
 
 
 ;;;; Environment
@@ -30,6 +30,8 @@
 ;;; Registers
 (set-register ?i
               (cons 'file (emacs-d "init.el")))
+(set-register ?p
+              (cons 'file (emacs-d "packages.el")))
 
 ;;; no backup files, no auto-saving
 ;;;--- TODO: Consolidate save files to a common directory.
@@ -155,190 +157,6 @@
 
 (when window-system
   (add-hook 'after-init-hook 'server-start t))
-
-
-
-
-
-(use-package ag
-  :ensure t
-  :defer t
-  :config
-  (progn
-    (setq ag-highlight-search t)
-    (bind-key "n" 'compilation-next-error ag-mode-map)
-    (bind-key "p" 'compilation-previous-error ag-mode-map)
-    (bind-key "N" 'compilation-next-file ag-mode-map)
-    (bind-key "P" 'compilation-previous-file ag-mode-map)))
-
-(use-package browse-kill-ring
-  :ensure t
-  :defer t
-  :config
-  (progn
-    (browse-kill-ring-default-keybindings)))
-
-(use-package diminish
-  :ensure t)
-
-(use-package edit-server
-  :ensure t
-  :if window-system
-  :init
-  (add-hook 'after-init-hook 'edit-server-start)
-  :config
-  (progn
-    (bind-key "C-c C-k" 'edit-server-abort edit-server-edit-mode-map)
-    (add-hook 'edit-server-start-hook
-              '(lambda ()
-                 (auto-fill-mode)
-                 (flyspell-mode)
-                 (set-fill-column 80)))))
-
-(use-package expand-region
-  :ensure t
-  :bind ("C-=" . er/expand-region))
-
-(use-package fill-column-indicator
-  :ensure t
-  :defer t)
-
-(use-package gist
-  :ensure t
-  :bind ("C-c g p" . gist-region-or-buffer-private))
-
-(use-package git-commit-mode
-  :ensure t
-  :defer t)
-
-(use-package gitconfig-mode
-  :ensure t
-  :defer t)
-
-(use-package gitignore-mode
-  :ensure t
-  :defer t)
-
-(use-package go-mode
-  :ensure t
-  :defer t
-  :init
-  (progn
-    (defun go-capitalize-previous-word ()
-      (interactive)
-      (backward-word)
-      (capitalize-word 1)))
-
-  :config
-  (progn
-    (bind-key "C-c C-c" 'go-capitalize-previous-word go-mode-map)
-    (bind-key "C-c f" 'gofmt go-mode-map)
-    (bind-key "C-c d" 'godoc go-mode-map)))
-
-(use-package google-c-style
-  :ensure t
-  :defer t)
-
-(use-package ido-ubiquitous
-  :ensure t
-  :init
-  ;; Fix ido-ubiquitous for newer packages
-  (defmacro ido-ubiquitous-use-new-completing-read (cmd package)
-    `(eval-after-load ,package
-       '(defadvice ,cmd (around ido-ubiquitous-new activate)
-          (let ((ido-ubiquitous-enable-compatibility nil))
-            ad-do-it))))
-  :config
-  (progn
-    (ido-ubiquitous-use-new-completing-read yas/expand 'yasnippet)
-    (ido-ubiquitous-use-new-completing-read yas/visit-snippet-file 'yasnippet)))
-
-(use-package magit
-  :ensure t
-  :bind ("C-x g" . magit-status)
-  :config
-  (progn
-    (setenv "GIT_PAGER" "")
-    (add-hook 'magit-log-edit-mode-hook
-              '(lambda ()
-                 (auto-fill-mode)
-                 (flyspell-mode)
-                 (set-fill-column 80)))))
-
-(use-package markdown-mode
-  :ensure t
-  :mode ("\\.\\(m\\(ark\\)?down\\|md\\)$" . markdown-mode)
-  :config
-  (progn
-    (let ((preferred-markdown-impl "peg-markdown"))
-      (when (executable-find preferred-markdown-impl)
-        (setq markdown-command preferred-markdown-impl)))
-
-    (add-hook 'markdown-mode-hook
-              '(lambda ()
-                 (auto-fill-mode)
-                 (flyspell-mode)))))
-
-(use-package multiple-cursors
-  :ensure t
-  :bind (("C-S-c C-S-c" . mc/edit-lines)
-         ("C->" . mc/mark-next-like-this)
-         ("C-<" . mc/mark-prvious-like-this)
-         ("C-c C-<" . mc/mark-all-like-this))
-  :config
-  (progn
-    (setq mc/list-file (emacs-d "var/multiple-cursors-all-or-once.el"))))
-
-(use-package mustache-mode
-  :ensure t
-  :defer t)
-
-(use-package org
-  :ensure t
-  :defer t
-  :config
-  (progn
-    (org-babel-do-load-languages
-     'org-babel-load-languages
-     '((awk . t)
-       (emacs-lisp . t)
-       (java . t)
-       (python . t)
-       (ruby . t)
-       (sh . t)))))
-
-(use-package protobuf-mode
-  :ensure t
-  :defer t)
-
-(use-package puppet-mode
-  :ensure t
-  :mode ("\\.pp$" . puppet-mode))
-
-(use-package regex-tool
-  :ensure t
-  :defer t)
-
-(use-package ruby-tools
-  :ensure t
-  :defer t)
-
-(use-package scala-mode2
-  :ensure t
-  :defer t)
-
-(use-package yaml-mode
-  :ensure t
-  :defer t)
-
-(use-package yasnippet
-  :ensure t
-  :diminish yas-minor-mode
-  :mode ("/\\.emacs\\.d/snippets/" . snippet-mode)
-  :init
-  (progn
-    (setq yas-verbosity 3)
-    (yas-global-mode 1)))
 
 
 (add-hook 'after-init-hook
---
+++ packages.el
@@ -0,0 +1,209 @@
+;;; load-path.el
+;;; External Packages
+(require 'package)
+(setq package-archives
+      (append package-archives
+              '(("melpa" . "http://melpa.milkbox.net/packages/"))
+              '(("org" . "http://orgmode.org/elpa/"))))
+
+;; Load the list of packages but don't initialize them.
+;; `use-package' will arrange the necessary autoload entries.
+(package-initialize nil)
+
+;; If never connected to repositories before, download package descriptions so
+;; `use-package' can trigger installation of missing packages.
+(unless package-archive-contents
+    (message "Refreshing ELPA package archives...")
+    (package-refresh-contents))
+
+;; ...but before everything, make sure `use-package' is installed.
+(unless (package-installed-p 'use-package)
+  (message "`use-package' not found.  Installing...")
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-minimum-reported-time 0)
+
+
+(use-package ag
+  :ensure t
+  :defer t
+  :config
+  (progn
+    (setq ag-highlight-search t)
+    (bind-key "n" 'compilation-next-error ag-mode-map)
+    (bind-key "p" 'compilation-previous-error ag-mode-map)
+    (bind-key "N" 'compilation-next-file ag-mode-map)
+    (bind-key "P" 'compilation-previous-file ag-mode-map)))
+
+(use-package browse-kill-ring
+  :ensure t
+  :defer t
+  :config
+  (progn
+    (browse-kill-ring-default-keybindings)))
+
+(use-package color-theme-sanityinc-tomorrow
+  :ensure t)
+
+(use-package diminish
+  :ensure t)
+
+(use-package edit-server
+  :ensure t
+  :if window-system
+  :init
+  (add-hook 'after-init-hook 'edit-server-start)
+  :config
+  (progn
+    (bind-key "C-c C-k" 'edit-server-abort edit-server-edit-mode-map)
+    (add-hook 'edit-server-start-hook
+              '(lambda ()
+                 (auto-fill-mode)
+                 (flyspell-mode)
+                 (set-fill-column 80)))))
+
+(use-package expand-region
+  :ensure t
+  :bind ("C-=" . er/expand-region))
+
+(use-package fill-column-indicator
+  :ensure t
+  :defer t)
+
+(use-package gist
+  :ensure t
+  :bind ("C-c g p" . gist-region-or-buffer-private))
+
+(use-package git-commit-mode
+  :ensure t
+  :defer t)
+
+(use-package gitconfig-mode
+  :ensure t
+  :defer t)
+
+(use-package gitignore-mode
+  :ensure t
+  :defer t)
+
+(use-package go-mode
+  :ensure t
+  :defer t
+  :init
+  (progn
+    (defun go-capitalize-previous-word ()
+      (interactive)
+      (backward-word)
+      (capitalize-word 1)))
+
+  :config
+  (progn
+    (bind-key "C-c C-c" 'go-capitalize-previous-word go-mode-map)
+    (bind-key "C-c f" 'gofmt go-mode-map)
+    (bind-key "C-c d" 'godoc go-mode-map)))
+
+(use-package google-c-style
+  :ensure t
+  :defer t)
+
+(use-package ido-ubiquitous
+  :ensure t
+  :init
+  ;; Fix ido-ubiquitous for newer packages
+  (defmacro ido-ubiquitous-use-new-completing-read (cmd package)
+    `(eval-after-load ,package
+       '(defadvice ,cmd (around ido-ubiquitous-new activate)
+          (let ((ido-ubiquitous-enable-compatibility nil))
+            ad-do-it))))
+  :config
+  (progn
+    (ido-ubiquitous-use-new-completing-read yas/expand 'yasnippet)
+    (ido-ubiquitous-use-new-completing-read yas/visit-snippet-file 'yasnippet)))
+
+(use-package magit
+  :ensure t
+  :bind ("C-x g" . magit-status)
+  :config
+  (progn
+    (setenv "GIT_PAGER" "")
+    (add-hook 'magit-log-edit-mode-hook
+              '(lambda ()
+                 (auto-fill-mode)
+                 (flyspell-mode)
+                 (set-fill-column 80)))))
+
+(use-package markdown-mode
+  :ensure t
+  :mode ("\\.\\(m\\(ark\\)?down\\|md\\)$" . markdown-mode)
+  :config
+  (progn
+    (let ((preferred-markdown-impl "peg-markdown"))
+      (when (executable-find preferred-markdown-impl)
+        (setq markdown-command preferred-markdown-impl)))
+
+    (add-hook 'markdown-mode-hook
+              '(lambda ()
+                 (auto-fill-mode)
+                 (flyspell-mode)))))
+
+(use-package multiple-cursors
+  :ensure t
+  :bind (("C-S-c C-S-c" . mc/edit-lines)
+         ("C->" . mc/mark-next-like-this)
+         ("C-<" . mc/mark-prvious-like-this)
+         ("C-c C-<" . mc/mark-all-like-this))
+  :config
+  (progn
+    (setq mc/list-file (emacs-d "var/multiple-cursors-all-or-once.el"))))
+
+(use-package mustache-mode
+  :ensure t
+  :defer t)
+
+(use-package org
+  :ensure t
+  :defer t
+  :config
+  (progn
+    (org-babel-do-load-languages
+     'org-babel-load-languages
+     '((awk . t)
+       (emacs-lisp . t)
+       (java . t)
+       (python . t)
+       (ruby . t)
+       (sh . t)))))
+
+(use-package protobuf-mode
+  :ensure t
+  :defer t)
+
+(use-package puppet-mode
+  :ensure t
+  :mode ("\\.pp$" . puppet-mode))
+
+(use-package regex-tool
+  :ensure t
+  :defer t)
+
+(use-package ruby-tools
+  :ensure t
+  :defer t)
+
+(use-package scala-mode2
+  :ensure t
+  :defer t)
+
+(use-package yaml-mode
+  :ensure t
+  :defer t)
+
+(use-package yasnippet
+  :ensure t
+  :diminish yas-minor-mode
+  :mode ("/\\.emacs\\.d/snippets/" . snippet-mode)
+  :init
+  (progn
+    (setq yas-verbosity 3)
+    (yas-global-mode 1)))
