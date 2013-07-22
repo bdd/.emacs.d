239486b02331dd2bee29ea917f89c1579abce125
Berk D. Demir <bdd@mindcast.org>
2013-07-22 00:31:23 -0700

    typo in `multiple-cursors' bind-key
    Correctly spell "previous".

--- packages.el
+++ packages.el
@@ -162,7 +162,7 @@
   :ensure t
   :bind (("C-S-c C-S-c" . mc/edit-lines)
          ("C->" . mc/mark-next-like-this)
-         ("C-<" . mc/mark-prvious-like-this)
+         ("C-<" . mc/mark-previous-like-this)
          ("C-c C-<" . mc/mark-all-like-this))
   :config
   (progn
