784ee5a5b88579ef9e4c9a132e78f00308091cfa
Berk D. Demir <bdd@mindcast.org>
2014-10-30 23:00:19 -0700

    better elisp hygiene in `bdd-defuns`
    Use `forward-line' instead of `goto-line' in `goto-line-with-feedback'.
    Add missing documentation strings for functions with obvious names.

--- bdd-defuns.el
+++ bdd-defuns.el
@@ -9,14 +9,16 @@
 (require 'bind-key)
 
 ;;; Code:
-
 (defun kill-region-or-backward-kill-word (arg)
   "If mark is active kill the region else backward kill word.
+With argument ARG, do this that many times.
 
-Traditionally Unix uses `C-w' for backward kill word.  Preserve Emacs default
-of kill-region if the mark is active, otherwise fallback to `backward-kill-word'.
-Also fix `backward-kill-word' so that it stops at whitespace."
+Traditionally Unix uses `C-w' for backward kill word.  Preserve
+Emacs default of `kill-region' if the mark is active, otherwise
+fallback to `backward-kill-word'.  Also fix `backward-kill-word'
+so that it stops at whitespace."
   (interactive "p")
+
 
   (defun backward-kill-word-without-spaces (arg)
     "Wrap backward-kill-word to swallow spaces separate from words."
@@ -40,6 +42,8 @@ Also fix `backward-kill-word' so that it stops at whitespace."
 
 
 (defun mark-line (&optional arg)
+  "Mark the line where point is.
+With argument ARG, do this that many times."
   (interactive "p")
   (or arg (setq arg 1))
   (beginning-of-line)
@@ -50,6 +54,8 @@ Also fix `backward-kill-word' so that it stops at whitespace."
 
 
 (defun mark-sentence (&optional arg)
+  "Mark the sentece around point.
+With argument ARG, do this that many times."
   (interactive "p")
   (or arg (setq arg 1))
   (backward-sentence)
@@ -59,6 +65,8 @@ Also fix `backward-kill-word' so that it stops at whitespace."
 
 
 (defun duplicate-line (&optional arg)
+  "Duplicate the where point is.
+With argument ARG, do this that many times."
   (interactive "p")
   (or arg (setq arg 1))
   (save-excursion
@@ -74,14 +82,17 @@ Also fix `backward-kill-word' so that it stops at whitespace."
 (bind-key "C-M-y" 'duplicate-line)
 
 
-;; from http://whattheemacsd.com/key-bindings.el-01.html
+;; originally from http://whattheemacsd.com/key-bindings.el-01.html
+;; modified to use `forward-line' because `goto-line' is interactive use only.
 (defun goto-line-with-feedback ()
   "Show line numbers temporarily, while prompting for the line number input."
   (interactive)
   (unwind-protect
       (progn
         (linum-mode 1)
-        (goto-line (read-number "Goto line: ")))
+        (let ((line (read-number "Go to line: " (line-number-at-pos))))
+          (goto-char (point-min))
+          (forward-line (1- line))))
     (linum-mode -1)))
 
 (global-set-key [remap goto-line] 'goto-line-with-feedback)
