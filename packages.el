8fc129e3b432e3fded6dc87ad66ed87dd00173e5
Berk D. Demir <bdd@mindcast.org>
2014-12-26 18:13:56 -0800

    New package: `thrift'
    Rather simple package, only syntax hightlighting from Thrift IDL files.

--- packages.el
+++ packages.el
@@ -277,6 +277,10 @@
     (setq smex-save-file (emacs-d "var/smex-items")))
   :bind ("M-x" . smex))
 
+(use-package thrift
+  :ensure t
+  :defer t)
+
 (use-package toml-mode
   :ensure t
   :defer t)
